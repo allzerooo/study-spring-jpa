9ac0a67590a5f2c6714d34df33be233fe1aa63bf
ddalam <dychoi.dev@gmail.com>
2022-07-04 22:29:37 +0900

    feature : 회원 조회 API 추가

--- src/main/java/study/springjpa/api/MemberApiController.java
+++ src/main/java/study/springjpa/api/MemberApiController.java
@@ -1,10 +1,13 @@
 package study.springjpa.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +21,17 @@ import study.springjpa.domain.service.MemberService;
 public class MemberApiController {
 
 	private final MemberService memberService;
+
+	@GetMapping("/api/v1/members")
+	public Result membersV1() {
+		final List<Member> members = memberService.findMembers();
+
+		final List<MemberResponse> collect = members.stream()
+				.map(m -> new MemberResponse(m.getName()))
+				.collect(Collectors.toList());
+
+		return new Result(collect);
+	}
 
 	@PostMapping("/api/v1/members")
 	public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request) {
@@ -34,6 +48,18 @@ public class MemberApiController {
 		memberService.update(id, request.getName());
 		Member findMember = memberService.findOne(id);
 		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+		private T data;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class MemberResponse {
+		private String name;
 	}
 
 	@Data
